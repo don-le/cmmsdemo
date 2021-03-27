9a607d4cd03794fb0a25b0861262534f56eacfb6
= <=>
2021-03-27 18:33:06 +0700

    Demo mockup

---
+++ BaiTap/src/views/Tables/Tables.js
@@ -0,0 +1,146 @@
+import React, { Component } from 'react';
+import { Badge, Card, CardBody, CardHeader, Col, Pagination, PaginationItem, PaginationLink, Row, Table } from 'reactstrap';
+import { NavLink } from 'react-router-dom'
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+import 'react-bootstrap-table/dist//react-bootstrap-table-all.min.css';
+import data from './_data';
+
+
+class Tables extends Component {
+  constructor(props) {
+    super(props);
+
+    this.table = data.rows;
+    this.options = {
+      sortIndicator: true,
+      hideSizePerPage: true,
+      paginationSize: 3,
+      hidePageListOnlyOnePage: true,
+      clearSearch: true,
+      alwaysShowAllBtns: false,
+      withFirstAndLast: false
+    }
+
+  }
+
+  // just an example
+  nameFormat(cell, row) {
+    const id = `/users/${row.id}`
+    return (
+      <NavLink strict to={id}> {cell} </NavLink>
+    );
+  };
+  render() {
+    return (
+      <div className="animated fadeIn">
+        <Row>
+          <Col xs="12" lg="12">
+            <Card>
+              <CardHeader>
+                <i className="fa fa-align-justify"></i> Simple Table
+              </CardHeader>
+              <CardBody>
+                <Table responsive>
+                  <thead>
+                    <tr>
+                      <th>Username</th>
+                      <th>Date registered</th>
+                      <th>Role</th>
+                      <th>Status</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>Samppa Nori</td>
+                      <td>2012/01/01</td>
+                      <td>Member</td>
+                      <td>
+                        <Badge color="success">Active</Badge>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>Estavan Lykos</td>
+                      <td>2012/02/01</td>
+                      <td>Staff</td>
+                      <td>
+                        <Badge color="danger">Banned</Badge>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>Chetan Mohamed</td>
+                      <td>2012/02/01</td>
+                      <td>Admin</td>
+                      <td>
+                        <Badge color="secondary">Inactive</Badge>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>Derick Maximinus</td>
+                      <td>2012/03/01</td>
+                      <td>Member</td>
+                      <td>
+                        <Badge color="warning">Pending</Badge>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>Friderik Dávid</td>
+                      <td>2012/01/21</td>
+                      <td>Staff</td>
+                      <td>
+                        <Badge color="success">Active</Badge>
+                      </td>
+                    </tr>
+                  </tbody>
+                </Table>
+                <Pagination>
+                  <PaginationItem>
+                    <PaginationLink previous tag="button"></PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem active>
+                    <PaginationLink tag="button">1</PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem>
+                    <PaginationLink tag="button">2</PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem>
+                    <PaginationLink tag="button">3</PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem>
+                    <PaginationLink tag="button">4</PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem>
+                    <PaginationLink next tag="button"></PaginationLink>
+                  </PaginationItem>
+                </Pagination>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col xs="12" lg="12">
+            <Card>
+              <CardHeader>
+                <i className="icon-menu"></i>Data Table{' '}
+                <a href="https://coreui.io/pro/react/" className="badge badge-danger">CoreUI Pro Component</a>
+                <div className="card-header-actions">
+                  <a href="https://github.com/AllenFang/react-bootstrap-table" rel="noopener noreferrer" target="_blank" className="card-header-action">
+                    <small className="text-muted">docs</small>
+                  </a>
+                </div>
+              </CardHeader>
+              <CardBody>
+                <BootstrapTable data={this.table} version="4" striped hover pagination search options={this.options}>
+                  <TableHeaderColumn dataField="name" dataSort dataFormat={this.nameFormat} >Name</TableHeaderColumn>
+                  <TableHeaderColumn isKey dataField="email">Email</TableHeaderColumn>
+                  <TableHeaderColumn dataField="age" dataSort>Age</TableHeaderColumn>
+                  <TableHeaderColumn dataField="city" dataSort>City</TableHeaderColumn>
+                </BootstrapTable>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+
+    );
+  }
+}
+
+export default Tables;
